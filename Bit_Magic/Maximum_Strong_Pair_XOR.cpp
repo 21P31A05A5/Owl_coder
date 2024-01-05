2d298b6bf54e2dd65f834603c44e445f7637dde5
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-05 22:35:47 +0530

    Create Maximum_Strong_Pair_XOR.cpp

---
+++ Bit_Magic/Maximum_Strong_Pair_XOR.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if((abs(arr[i]-arr[j]))<=(min(arr[i],arr[j])))
+            {
+                ans=max(ans,(arr[i]^arr[j]));
+            }
+        }
+    }
+    cout<<ans;
+}
