e6ff7f47b4f1eab13070de1828da0cac4e92b5eb
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-11 22:04:09 +0530

    Create Difference of Primes and Composites.cpp

---
+++ Seive/Difference of Primes and Composites.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<bool> primes;
+void generate_seive(int n)
+{
+    // int n=1e6+1;
+    primes.assign(n+1, true);
+    primes[0] = primes[1] = false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (primes[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                primes[j] = false;
+            }
+        }
+    }
+}
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    generate_seive(n);
+    int prime=0;
+    int composite=0;
+    for(int i=m;i<=n;i++)
+    {
+        if(i==1 || i==0)
+        {
+            continue;
+        }
+        else if(primes[i])
+        {
+            prime++;
+        }
+        else
+        {
+            composite++;
+        }
+    }
+    cout<<composite-prime;
+}
