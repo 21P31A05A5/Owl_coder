24444c3ac86543f3be1d9d1f3fc2e1b4ff9e3e3a
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2024-01-14 21:44:30 +0530

    Create Solve The Equation.cpp

---
+++ Math/Solve The Equation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a;
+        cin>>a;
+        a=a-1;
+        int ans=sqrt(a);
+        cout<<ans<<endl;
+    }
+}
